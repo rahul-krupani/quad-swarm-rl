bd764cc5cbfd83d7ba8bd4f2fd0881706e309448
rahul-krupani <rahul.krupani@outlook.com>
2024-08-05 16:10:16 -0700

    Add noise to obstacle obs

--- gym_art/quadrotor_multi/obstacles/obstacles.py
+++ gym_art/quadrotor_multi/obstacles/obstacles.py
@@ -16,6 +16,7 @@ class MultiObstacles:
         self.fov_angle = 45 * np.pi / 180
         self.scan_angle_arr = np.array([0., np.pi/2, np.pi, -np.pi/2])
         self.num_rays = obst_tof_resolution
+        self.angle_noise_std = 0.1
 
     def reset(self, obs, quads_pos, pos_arr, quads_rots=None):
         self.pos_arr = copy.deepcopy(np.array(pos_arr))
@@ -26,9 +27,10 @@ class MultiObstacles:
                                               quads_sdf_obs=quads_sdf_obs, obst_radius=self.obstacle_radius,
                                               resolution=self.resolution)
         else:
+            noise_angles = self.scan_angle_arr + np.random.normal(loc=0, scale=self.angle_noise_std, size=self.scan_angle_arr.shape)
             quads_sdf_obs = get_ToFs_depthmap(quad_poses=quads_pos, obst_poses=self.pos_arr,
                                               obst_radius=self.obstacle_radius, scan_max_dist=2.0,
-                                              quad_rotations=quads_rots, scan_angle_arr=self.scan_angle_arr,
+                                              quad_rotations=quads_rots, scan_angle_arr=noise_angles,
                                               fov_angle=self.fov_angle, num_rays=self.num_rays, obst_noise=self.obst_noise)
 
         obs = np.concatenate((obs, quads_sdf_obs), axis=1)
@@ -42,9 +44,10 @@ class MultiObstacles:
                                               quads_sdf_obs=quads_sdf_obs, obst_radius=self.obstacle_radius,
                                               resolution=self.resolution)
         else:
+            noise_angles = self.scan_angle_arr + np.random.normal(loc=0, scale=self.angle_noise_std, size=self.scan_angle_arr.shape)
             quads_sdf_obs = get_ToFs_depthmap(quad_poses=quads_pos, obst_poses=self.pos_arr,
                                               obst_radius=self.obstacle_radius, scan_max_dist=2.0,
-                                              quad_rotations=quads_rots, scan_angle_arr=self.scan_angle_arr,
+                                              quad_rotations=quads_rots, scan_angle_arr=noise_angles,
                                               fov_angle=self.fov_angle, num_rays=self.num_rays, obst_noise=self.obst_noise)
 
         obs = np.concatenate((obs, quads_sdf_obs), axis=1)
